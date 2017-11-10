20ab9a9bf6263f0865fd7b22448a324127b64147
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2017-11-10 11:42:02 +0100

    improved processing of complex data and moved calc functions to Timeseries

--- dataprocessing/readtools.py
+++ dataprocessing/readtools.py
@@ -31,16 +31,38 @@ def read_timeseries_PLECS(filename, timeseries_names=None):
             timeseries_list.append(TimeSeries(name, pd_df['Time'].values, pd_df[name].values))
     return timeseries_list
 
-def read_timeseries_DPsim(filename, timeseries_names=None):
-    pd_df = pd.read_csv(filename)
+def read_timeseries_dpsim_real(filename, header=None, timeseries_names=None):
+    """Reads real time series data from DPsim log file which may have a header.
+    Timeseries names are assigned according to the header names if available.
+    :param filename: name of the csv file that has the data
+    :param header: specifies if the log file has a header
+    :param timeseries_names: column names which should be read
+    :return: list of Timeseries objects
+    """
     timeseries_list = []
+
+    if header is True:
+        pd_df = pd.read_csv(filename)
+    else:
+        pd_df = pd.read_csv(filename, header=None)
 
     if timeseries_names is None:
         # No trajectory names specified, thus read in all
-        timeseries_names = list(pd_df.columns.values)
-        timeseries_names.remove('Time')
-        for name in timeseries_names:
-            timeseries_list.append(TimeSeries(name, pd_df['Time'].values, pd_df[name].values))
+        column_names = list(pd_df.columns.values)
+        # Remove timestamps column name and store separately
+        column_names.remove(0)
+        timestamps = pd_df.iloc[:,0]
+
+        if header is True:
+            for name in column_names:
+                timeseries_list.append(TimeSeries(name, timestamps, pd_df[name].values))
+        else:
+            node_number = int(len(column_names))
+            node_index = 1
+            for column in column_names:
+                ts_name = 'node ' + str(node_index)
+                timeseries_list.append(TimeSeries(ts_name, timestamps, pd_df.iloc[:, column]))
+                node_index = node_index + 1
     else:
         # Read in specified time series
         print('no column names specified yet')
@@ -51,23 +73,68 @@ def read_timeseries_DPsim(filename, timeseries_names=None):
         print(result.name)
     return timeseries_list
 
-def read_timeseries_DPsim_node_values(filename, timeseries_names=None):
+def read_timeseries_dpsim_cmpl(filename, timeseries_names=None):
+    """Reads complex time series data from DPsim log file. Real and
+    imaginary part are stored in one complex variable.
+    :param filename: name of the csv file that has the data
+    :param timeseries_names: column name which should be read
+    :return: list of Timeseries objects
+    """
     pd_df = pd.read_csv(filename, header=None)
     timeseries_list = []
 
     if timeseries_names is None:
         # No trajectory names specified, thus read in all
         column_names = list(pd_df.columns.values)
+        # Remove timestamps column name and store separately
         column_names.remove(0)
-        node_index = 1
+        timestamps = pd_df.iloc[:,0]
+        # Calculate number of network nodes since array is [real, imag]
         node_number = int(len(column_names) / 2)
+        node_index = 1
         for column in column_names:
             if node_index <= node_number:
-                node_name = node_index
-                timeseries_list.append(TimeSeries('node '+ str(node_name) +' Re', pd_df.iloc[:,0], pd_df.iloc[:,column]))
+                ts_name = 'node '+ str(node_index)
+                timeseries_list.append(TimeSeries(ts_name, timestamps, np.vectorize(complex)(pd_df.iloc[:,column],pd_df.iloc[:,column + node_number])))
             else:
-                node_name = node_index - node_number
-                timeseries_list.append(TimeSeries('node '+ str(node_name) +' Im', pd_df.iloc[:,0], pd_df.iloc[:,column]))
+                break
+            node_index = node_index + 1
+    else:
+        # Read in specified time series
+        print('cannot read specified columns yet')
+
+    print('DPsim results file length:')
+    print(len(timeseries_list))
+    for result in timeseries_list:
+        print(result.name)
+    return timeseries_list
+
+def read_timeseries_dpsim_cmpl_separate(filename, timeseries_names=None):
+    """Deprecated - Reads complex time series data from DPsim log file. Real and
+    imaginary part are stored separately.
+    :param filename: name of the csv file that has the data
+    :param timeseries_names: column name which should be read
+    :return: list of Timeseries objects
+    """
+    pd_df = pd.read_csv(filename, header=None)
+    timeseries_list = []
+
+    if timeseries_names is None:
+        # No trajectory names specified, thus read in all
+        column_names = list(pd_df.columns.values)
+        # Remove timestamps column name and store separately
+        column_names.remove(0)
+        timestamps = pd_df.iloc[:, 0]
+        # Calculate number of network nodes since array is [real, imag]
+        node_number = int(len(column_names) / 2)
+        node_index = 1
+        for column in column_names:
+            if node_index <= node_number:
+                node_name = 'node '+ str(node_index) +' Re'
+                timeseries_list.append(TimeSeries(node_name, timestamps, pd_df.iloc[:,column]))
+            else:
+                node_name = 'node '+ str(node_index - node_number) +' Im'
+                timeseries_list.append(TimeSeries(node_name, timestamps, pd_df.iloc[:,column]))
 
             node_index = node_index + 1
     else:
