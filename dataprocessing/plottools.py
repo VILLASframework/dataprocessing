cf5a85abae4f7c07b6451c9db84212bd49b2b8ca
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2017-11-13 14:21:20 +0100

    improved input handling and added comments

--- dataprocessing/plottools.py
+++ dataprocessing/plottools.py
@@ -3,20 +3,38 @@ import numpy as np
 from .timeseries import *
 
 
-def plot_timeseries(figure_id, timeseries, plt_linestyle='-'):
+def plot_timeseries(figure_id, timeseries, plt_linestyle='-', plt_linewidth=2, plt_color=None):
+    """
+    This function plots either a single timeseries or several timeseries in the figure defined by figure_id.
+    Several timeseries (handed over in a list) are plotted in several subplots.
+    In order to plot several timeseries in one plot, the function is to be called several times (hold is activated).
+    """
     plt.figure(figure_id)
-
     if not isinstance(timeseries, list):
-        plt.plot(timeseries.time, timeseries.values, linestyle=plt_linestyle, label=timeseries.label)
+        if plt_color:
+            plt.plot(timeseries.time, timeseries.values, linestyle=plt_linestyle, label=timeseries.label, linewidth=plt_linewidth, color=plt_color)
+        else:
+            plt.plot(timeseries.time, timeseries.values, linestyle=plt_linestyle, label=timeseries.label, linewidth=plt_linewidth)
         plt.gca().autoscale(axis='x', tight=True)
         plt.legend()
     else:
         for ts in timeseries:
             plt.subplot(len(timeseries), 1, timeseries.index(ts) + 1)
-            plt.plot(ts.time, ts.values, linestyle=plt_linestyle, label=ts.label)
+            if plt_color:
+                plt.plot(ts.time, ts.values, linestyle=plt_linestyle, label=ts.label, linewidth=plt_linewidth, color=plt_color)
+            else:
+                plt.plot(ts.time, ts.values, linestyle=plt_linestyle, label=ts.label, linewidth=plt_linewidth)
             plt.gca().autoscale(axis='x', tight=True)
             plt.legend()
 
-def set_time_series_labels(timeseries_list, time_series_labels):
-    for ts in timeseries_list:
-        ts.label = time_series_labels[timeseries_list.index(ts)]
+
+def set_timeseries_labels(timeseries, timeseries_labels):
+    """
+    Sets label attribute of timeseries, later used in plotting functions.
+    Suitable for single timeseries as well as for several timeseries (handed over in a list).
+    """
+    if not isinstance(timeseries, list):
+        timeseries.label = timeseries_labels
+    else:
+        for ts in timeseries:
+            ts.label = timeseries_labels[timeseries.index(ts)]
