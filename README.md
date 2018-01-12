5cc4d322834ff455d774a8b40f377d16a1c30891
Steffen Vogel <post@steffenvogel.de>
2018-01-12 13:20:56 +0100

    updated setup.py and added README

---
+++ README.md
@@ -0,0 +1,38 @@
+# Dataprocessing toolkit for RWTH ACS simulators
+
+## Copyright
+
+2017, Institute for Automation of Complex Power Systems, EONERC, RWTH Aachen University
+
+## License
+
+This project is released under the terms of the [GPL version 3](COPYING.md).
+
+```
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+```
+
+For other licensing options please consult [Prof. Antonello Monti](mailto:amonti@eonerc.rwth-aachen.de).
+
+## Contact
+
+[![EONERC ACS Logo](doc/eonerc_logo.png)](http://www.acs.eonerc.rwth-aachen.de)
+
+- Markus Mirz <mmirz@eonerc.rwth-aachen.de>
+- Jan Dinkelbach <JDinkelbach@eonerc.rwth-aachen.de>
+- Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
+
+[Institute for Automation of Complex Power Systems (ACS)](http://www.acs.eonerc.rwth-aachen.de)
+[EON Energy Research Center (EONERC)](http://www.eonerc.rwth-aachen.de)
+[RWTH University Aachen, Germany](http://www.rwth-aachen.de)
--- setup.py
+++ setup.py
@@ -1,7 +1,45 @@
+import os
 from setuptools import setup, find_packages
 
-setup(name="dataprocessing",
-      version="0.1",
-	  packages=find_packages(),
+def read(fname):
+    dname = os.path.dirname(__file__)
+    fname = os.path.join(dname, fname)
+
+    try:
+        import m2r
+        return m2r.parse_from_file(fname)
+    except ImportError:
+        with open(fname) as f:
+            return f.read()
+
+setup(
+    name = "acs-dataprocessing",
+    version = "0.1.1",
+    author = "Markus Mirz, Jan Dinkelbach, Steffen Vogel",
+    author_email = "acs-software@eonerc.rwth-aachen.de",
+    description = "Several tools for processing simulation results",
+    license = "GPL-3.0",
+    keywords = "simulation power system real-time data processing",
+    url = "https://git.rwth-aachen.de/acs/public/simulation/data-processing",
+    packages = find_packages(),
+    long_description = read('README.md'),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3"
+    ],
+    install_requires = [
+        "matplotlib",
+        "numpy",
+        "pandas"
+    ],
+    setup_requires = [
+        'm2r',
+        'wheel'
+    ]
 )
 
