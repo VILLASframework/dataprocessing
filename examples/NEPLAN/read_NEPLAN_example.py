48704105ddb173afc3c918e4299b56f9292b8161
Bichen Li <bichen.li@eonerc.rwth-aachen.de>
2018-03-27 13:42:33 +0200

    -Fix Bugs when read in specific variables -Add "_loadflow" suffix to read_in function -Add some comments in the function

--- examples/NEPLAN/read_NEPLAN_example.py
+++ examples/NEPLAN/read_NEPLAN_example.py
@@ -9,7 +9,8 @@ file = r"C:\Users\admin\Desktop\Load_read\Load_flow_WCSS.rlf"
 
 # Example 1: Read in all variable
 print('************************ Test for read in all variable start ****************')
-result = read_timeseries_NEPLAN(file)
+result = read_timeseries_NEPLAN_loadflow(file)
+print(result[24].values)
 for i in range(len(result)):
     print('%s is %s' % (result[i].name, result[i].values)) # result as list of TimeSeries
 print('************************ Test for read in all variable end ****************')
@@ -21,12 +22,12 @@ print('\n')
 # Example 2: Read in specific variable
 print('************************ Test for read in specific variable start ****************')
 print('************************ Read in specific Voltage ****************')
-result2 = read_timeseries_NEPLAN(file, 'FOUR.U')
+result2 = read_timeseries_NEPLAN_loadflow(file, 'FOUR.U')
 for i in range(len(result2)):
     print('%s is %s' % (result2[i].name, result2[i].values))
 
 print('************************ Read in specific Current ****************')
-result3 = read_timeseries_NEPLAN(file, 'LINE89.I')
+result3 = read_timeseries_NEPLAN_loadflow(file, 'LINE89.I')
 for i in range(len(result3)):
     print('%s is %s' % (result3[i].name, result3[i].values))
 print('************************ Test for read in specific variable end ****************')
@@ -36,7 +37,7 @@ print('\n')
 
 # Example 3: Read in using regular expression
 print('************************ Test for read in using Regular Expression start ****************')
-result4 = read_timeseries_NEPLAN(file, '^LINE89.*$', True)
+result4 = read_timeseries_NEPLAN_loadflow(file, '^.*\.I$', True)
 for i in range(len(result4)):
     print('%s is %s' % (result4[i].name, result4[i].values))
 print('************************ Test for read in using Regular Expression end ****************')
