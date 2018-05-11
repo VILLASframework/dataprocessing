528e66b9fc7dee230f04d39d9df1302001e2a4f1
Bichen Li <libichen94@gmail.com>
2018-05-11 21:23:01 +0200

    Fix the bugs on path of the Result files

--- Assert_Results.py
+++ Assert_Results.py
@@ -7,7 +7,7 @@ from dataprocessing.readtools import *
 print("Test Start")
 # We need to extract all the result files from git now
 for files in os.listdir(
-        os.path.abspath(os.path.abspath(os.path.join(os.getcwd(), "..")) + "/reference-results/Neplan/BasicGrids")):
+        os.path.abspath("reference-results/Neplan/BasicGrids")):
     print(files)
     assert_modelia_neplan_results(os.path.splitext(files)[0]) #  Assert the result, model result path read from cmd line
 print("Test End")
