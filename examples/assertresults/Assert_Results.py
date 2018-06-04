1ce8d543ac686c3ad614cbc95d35f60d550f320c
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2018-06-04 16:28:46 +0200

    restructuring examples scripts

---
+++ examples/assertresults/Assert_Results.py
@@ -0,0 +1,18 @@
+import re
+import os
+import sys
+
+from dataprocessing.Validationtools import *
+from dataprocessing.readtools import *
+print("Test Start")
+# We need to extract all the result files from git now
+for files in os.listdir(
+        os.path.abspath("reference-results/Neplan/BasicGrids")):
+        #  Assert the result, model result path read from cmd line
+        assert_modelia_neplan_results(os.path.splitext(files)[0],
+                                      os.path.abspath("reference-results/Modelica/BasicGrids/" +
+                                                      os.path.splitext(files)[0] + ".mat"),
+                                      os.path.abspath("reference-results/Neplan/BasicGrids/" +
+                                                      os.path.splitext(files)[0] + ".rlf"))
+
+print("Test End")
