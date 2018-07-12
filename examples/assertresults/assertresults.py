280bb40d93e238e3934b3a285eb17d899e4c434e
Bichen Li <libichen94@gmail.com>
2018-07-12 13:44:24 +0200

    - Add a simulate_modelica function for simulation the model

--- examples/assertresults/assertresults.py
+++ examples/assertresults/assertresults.py
@@ -3,9 +3,32 @@ import os
 import sys
 
 sys.path.append(r"D:\HIWI\Git\data-processing\dataprocessing")
+sys.path.append(r"D:\HIWI\Git\python-for-modelica-dev_interface\Py4Mod\py4mod")
+
+from ModelicaModel import ModelicaModel
 from validationtools import *
 from readtools import *
 os.chdir(r"D:\HIWI\Git")
+
+
+def simulate_modelica(model_name, model_path):
+    interface = ModelicaModel(model_name, model_path)
+
+    # Initialization
+    interface.createInterface("OPENMODELICA")
+    interface.loadFile(model_path + '\ModPowerSystems\package.mo')
+
+    # Redirection
+    cwd = os.getcwd()
+    wd = os.path.join(cwd, 'test')
+    if not os.path.exists(wd):
+        os.makedirs(wd)
+    interface.changeWorkingDirectory(wd.replace("\\", "/"))
+
+    # Build & Run
+    interface.buildModel()
+    interface.simulate()
+
 
 print("Test Start")
 # We need to extract all the result files from git now
