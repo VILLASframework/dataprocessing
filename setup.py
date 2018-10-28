b8de0fa63c3eee9ab295c3340950709788082761
Steffen Vogel <post@steffenvogel.de>
2018-10-28 14:02:52 +0100

    code style in setup.py

--- setup.py
+++ setup.py
@@ -22,29 +22,29 @@ def read(fname):
             return sanatized
 
 setup(
-    name = "villas-dataprocessing",
-    version = "0.2.0",
-    author = "Markus Mirz, Jan Dinkelbach, Steffen Vogel",
-    author_email = "acs-software@eonerc.rwth-aachen.de",
-    description = "Several tools for processing simulation results",
-    license = "GPL-3.0",
-    keywords = "simulation power system real-time data processing",
-    url = "https://git.rwth-aachen.de/acs/public/simulation/data-processing",
-    packages = [ "villas.dataprocessing" ],
+    name = 'villas-dataprocessing',
+    version = '0.2.0',
+    author = 'Markus Mirz, Jan Dinkelbach, Steffen Vogel',
+    author_email = 'acs-software@eonerc.rwth-aachen.de',
+    description = 'Several tools for processing simulation results',
+    license = 'GPL-3.0',
+    keywords = 'simulation power system real-time data processing',
+    url = 'https://git.rwth-aachen.de/acs/public/villas/dataprocessing',
+    packages = [ 'villas.dataprocessing' ],
     long_description = read('README.md'),
     classifiers = [
-        "Development Status :: 4 - Beta",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3"
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3'
     ],
     install_requires = [
-        "matplotlib",
-        "numpy",
-        "pandas"
+        'matplotlib',
+        'numpy',
+        'pandas'
     ],
     setup_requires = [
         'm2r',
