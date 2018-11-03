935716c09656700234555b8e3d8db80ca8e5650f
Steffen Vogel <post@steffenvogel.de>
2018-11-03 21:00:51 +0100

    bump version to v0.2.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(fname):
 
 setup(
     name = 'villas-dataprocessing',
-    version = '0.2.1',
+    version = '0.2.2',
     author = 'Markus Mirz, Jan Dinkelbach, Steffen Vogel',
     author_email = 'acs-software@eonerc.rwth-aachen.de',
     description = 'Several tools for processing simulation results',
