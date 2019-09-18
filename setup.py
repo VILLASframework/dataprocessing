110b3ca87cf14e4a341a1f7b6774dc897df6c67b
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2019-09-18 14:33:29 +0200

    update package setup files

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     author = 'Markus Mirz, Jan Dinkelbach, Steffen Vogel',
     author_email = 'acs-software@eonerc.rwth-aachen.de',
     description = 'Several tools for processing simulation results',
+    long_description_content_type='text/markdown',
     license = 'GPL-3.0',
     keywords = 'simulation power system real-time data processing',
     url = 'https://git.rwth-aachen.de/acs/public/villas/dataprocessing',
