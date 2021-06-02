0cf947f728d07023c38174d721c024cee072f81f
Steffen Vogel <post@steffenvogel.de>
2021-06-02 11:56:07 +0200

    add support IPython.display

--- villas/web/file.py
+++ villas/web/file.py
@@ -1,4 +1,16 @@
 import requests
+import pandas
+import scipy.io.wavfile
+import scipy.io
+import numpy
+import json
+import os
+import IPython.display as ipyd
+import IPython.core.formatters as ipyf
+import pygments.lexers as pygl
+import pygments.util as pygu
+import mimetypes
+
 from zipfile import ZipFile
 from dateutil.parser import parse
 import tempfile
@@ -9,7 +21,6 @@ class File:
         self.id = data.get('id')
         self.token = token
         self.endpoint = endpoint
-
 
         self.name = data.get('name')
         self.scenario_id = data.get('scenarioID')
@@ -23,6 +34,77 @@ class File:
 
     def __repr__(self):
         return f'<villas.web.file.File id={self.id} name={self.name} size={self.size} type={self.type}>'
+
+    def load(self, *args, **kwargs):
+        with self.open(download=True) as f:
+            if   self.type == 'text/csv':
+                return pandas.read_csv(f)
+            elif self.type == 'application/json':
+                return json.load(f)
+            elif self.type in ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']:
+                return pandas.read_excel(f, *args, **kwargs)
+            elif self.type == 'application/x-hdf5':
+                return pandas.read_hdf(f, *args, **kwargs)
+            elif self.type == 'application/x-matlab-data':
+                mat = scipy.io.loadmat(f)
+                mat = {k: v for k, v in mat.items() if k[0] != '_'}
+                return pandas.DataFrame({k: numpy.array(v).flatten() for k, v in mat.items()})
+            else:
+                return f.read()
+
+    def _repr_mimebundle_(self, include, exclude):
+        data = self.load()
+
+        if self.type == 'application/octet-stream':
+            type, encoding = mimetypes.guess_type(self.name, strict=False)
+            if type is None:
+                fn, ext = os.path.splitext(self.name)
+
+                if ext == '.md':
+                    self.type = 'text/markdown'
+            else:
+                self.type = type
+
+        if isinstance(data, pandas.DataFrame):
+            fdata = data # DataFrame already can format itself
+        elif self.type in ['image/png', 'image/gif', 'image/jpeg']:
+            fdata = ipyd.Image(data=data)
+        elif self.type == 'image/svg+xml':
+            fdata = ipyd.SVG(data=data)
+        elif self.type.startswith('audio/'):
+            self.temp = tempfile.NamedTemporaryFile('wb+')
+            self.temp.write(data)
+            self.temp.flush()
+            fdata = ipyd.Audio(self.temp.name)
+        elif self.type == 'text/html':
+            fdata = ipyd.HTML(data.decode('utf-8'))
+        elif self.type == 'application/javascript':
+            return ipyd.Javascript(data)
+        elif self.type == 'application/json':
+            fdata = ipyd.JSON(data)
+        elif self.type == 'text/markdown':
+            fdata = ipyd.Markdown(data.decode('utf-8'))
+        elif self.type in ['application/x-latex', 'application/x-tex']:
+            fdata = ipyd.Latex(data.decode('utf-8'))
+
+        elif self.type in ['text/plain']:
+            try:
+                lexer = pygl.get_lexer_for_filename(self.name)
+                print(lexer, lexer.aliases[0])
+                fdata = ipyd.Code(data=data.decode('utf-8'), language=lexer.aliases[0])
+            except pygu.ClassNotFound:
+                if self.type == 'text/plain':
+                    fdata = ipyd.TextDisplayObject(data)
+                else:
+                    fdata = data
+        else:
+            try:
+                lexer = pygl.get_lexer_for_mimetype(self.type)
+                fdata = ipyd.Code(data=data.decode('utf-8'), language=lexer.aliases[0])
+            except pygu.ClassNotFound:
+                fdata = data
+
+        return ipyf.format_display_data(fdata)
 
     def open(self, download=False):
         resp = requests.request('GET',
